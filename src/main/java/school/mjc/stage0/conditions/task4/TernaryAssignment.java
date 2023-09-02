d1a4491fc4589a42db4367df7f13f5fdcf4337fd
Zhakshylyk Kasymbekov <jakkasymbekov@gmail.com>
2023-09-02 19:29:14 +0600

    Ternary conditions tasks commit after the first testing

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
@@ -2,8 +2,7 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryAssignment {
     public void assignAndPrintBasedOnWhichBigger(int first, int second) {
-        int result = 0;
-        result = first > second ? 10 : -10;
+        int result = first > second ? 10 : -10;
         System.out.println(result);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
@@ -2,6 +2,6 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryPrinter {
     public void printWhichIsBigger(int first, int second) {
-        System.out.println(first > second ? first : second);
+        System.out.println(first > second ? "first" : "second");
     }
 }
